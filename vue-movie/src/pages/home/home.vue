ff68096f32b04ed647d78fa9abc99e4d053aaf57
PeiXueYang <pei1825540@126.com>
2018-06-27 14:22:42 +0800

    第一次提交

---
+++ vue-movie/src/pages/home/home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <div>
+      home
+    </div>
+  </div>
+</template>
+<script>
+  import {
+    getMovies
+  } from '../../api/movie.js'
+export default {
+  data(){
+    return {
+
+    }
+  },
+  methods: {
+      getMovieInfo() {
+        // /api/movie/in_theaters
+        getMovies().then(res=>{
+           console.log(res,'res')
+        })
+
+      }
+
+    },
+    created() {
+      this.getMovieInfo()
+    }
+}
+</script>
+
